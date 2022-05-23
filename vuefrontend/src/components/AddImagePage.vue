6ae4b2c373c75161a37733422685f71d68ad3e77
I.E.U. Juboraj Naofel <57951503+juborajnaofel@users.noreply.github.com>
2022-05-23 19:03:22 +0600

    larabackend base

--- vuefrontend/src/components/AddImagePage.vue
+++ vuefrontend/src/components/AddImagePage.vue
@@ -4,7 +4,7 @@
     <br>
     <div class="form_div">
         <input @input="fetchChangeValue" v-model="url" type="text" placeholder="Enter Image URL" /><br>
-        <img :src="url = this.url" width = "300" height = "250" />
+        <img src="" width = "300" height = "250" />
         <button v-on:click="logFun">Submit</button>
     </div>
 </template>
